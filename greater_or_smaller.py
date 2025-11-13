9d7da0707bb59efe092050474af57c2bc7d13400
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-11-13 19:39:28 +0530

    Add files via upload

---
+++ greater_or_smaller.py
@@ -0,0 +1,8 @@
+i = int(input("Please enter any integer number : "))
+if i<=15:
+   print("the number is either smaller than 15 or equal to 15")
+   print("This statement is under the if condition")
+else:
+   print("the number is greater than 15")
+   print("This statement is under the else condition")
+print("This statement is not in if or else block it prints no matter the result of the condition")
