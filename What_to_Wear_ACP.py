44e9c8ca117512a4bfa99f1f73a4cf334ed54502
VivaanMahapatra <vivaanmahapatra51@gmail.com>
2025-11-13 19:51:30 +0530

    Add files via upload

---
+++ What_to_Wear_ACP.py
@@ -0,0 +1,5 @@
+temp = int(input("Enter the temperature at the location you are at right now in degree Celcius :  "))
+if temp<=22:
+  print("You will need to wear warm clothes such as, thermals, thick shirt, wool sweater, coat, woolen cap, woolen gloves, boots, and trousers")
+else:
+  print("You can wear light clothes such as a simple, shirt, tshirt, shorts, pants, shoes")
